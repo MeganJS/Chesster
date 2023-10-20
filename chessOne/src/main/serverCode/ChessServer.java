c66aa484815980cc68c8419a2fcc9e8259379cec
MeganJS <247megamania@gmail.com>
2023-10-20 15:12:10 -0600

    Started work on ChessServer

--- chessOne/src/main/serverCode/ChessServer.java
+++ chessOne/src/main/serverCode/ChessServer.java
@@ -1,4 +1,18 @@
 package serverCode;
 
+import spark.Spark;
+import spark.Request;
+import spark.Response;
+
 public class ChessServer {
+    public static void main(String[] args) {
+        Spark.port(8080);
+        Spark.externalStaticFileLocation("web");
+
+    }
+
+    private static void createRoutes() {
+
+    }
+
 }
---
+++ chessOne/src/test/myTests/myServerTests.java
@@ -0,0 +1,14 @@
+package myTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class myServerTests {
+    @Test
+    public void testTheTesting() {
+        assertEquals(300, 200 + 100);
+    }
+    
+
+}
