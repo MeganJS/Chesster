7fda15dd4cba0d997ae1042567c70489ebbb3109
MeganJS <247megamania@gmail.com>
2023-10-25 23:11:46 -0600

    set up rest of LoginHandler, not yet tested

--- chessOne/src/main/serverCode/handlers/LoginHandler.java
+++ chessOne/src/main/serverCode/handlers/LoginHandler.java
@@ -1,28 +1,60 @@
 package serverCode.handlers;
 
 import com.google.gson.Gson;
+import dataAccess.DataAccessException;
+import serverCode.models.AuthToken;
 import serverCode.models.User;
 import spark.Request;
 import spark.Response;
 
+import java.io.IOException;
 import java.util.Map;
+
+import static serverCode.services.UserAuthServices.login;
 
 public class LoginHandler {
 
     public static Object handleLogin(Request req, Response res) {
         var serializer = new Gson();
         var userMap = serializer.fromJson(req.body(), Map.class);
-        String password = "";
-        String username = "";
-        if (userMap.containsKey("username")) {
-            username = (String) userMap.get("username");
+        var returnedUser = createUser(userMap);
+        if (returnedUser.getClass() == ErrorDescription.class) {
+            res.status(401);
+            res.body(serializer.toJson(returnedUser));
+            return res.body();
         }
-        if (userMap.containsKey("password")) {
-            password = (String) userMap.get("password");
+        User userToLog = (User) returnedUser;
+        try {
+            AuthToken authToken = login(userToLog);
+            res.status(200);
+            res.body(serializer.toJson(userToLog));
+            return res.body();
+        } catch (Exception ex) {
+            res.status(401);
+            res.body(serializer.toJson(new ErrorDescription("Error: unauthorized")));
+            return res.body();
         }
-        User userToLog = new User(username, password, null);
-        res.body(serializer.toJson(userToLog));
-        res.status(200);
-        return res.body();//turns out req.body() and res.body() are already Json objects!
+    }
+
+    private static Object createUser(Map userMap) {
+        try {
+            String password = checkForString(userMap, "password");
+            String username = checkForString(userMap, "username");
+            String email = checkForString(userMap, "email");
+            return new User(username, password, email);
+        } catch (IOException ex) {
+            return new ErrorDescription("Error: unauthorized");
+        }
+    }
+
+    private static String checkForString(Map userMap, String key) throws IOException {
+        if (userMap.containsKey(key)) {
+            if (userMap.get(key) != null && !userMap.get(key).equals("")) {
+                return (String) userMap.get(key);
+            } else {
+                throw new IOException();
+            }
+        }
+        throw new IOException();
     }
 }
